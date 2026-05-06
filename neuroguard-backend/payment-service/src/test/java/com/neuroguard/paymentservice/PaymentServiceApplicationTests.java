e3d842ac8651d7105f6b0fe16841aa5d2950213d
nour trabelsi <144689582+trabelsiNour123@users.noreply.github.com>
2026-05-06 23:59:37 +0100

    final fix CI pipeline (junit + mvnw + stability)5

--- neuroguard-backend/payment-service/src/test/java/com/neuroguard/paymentservice/PaymentServiceApplicationTests.java
+++ neuroguard-backend/payment-service/src/test/java/com/neuroguard/paymentservice/PaymentServiceApplicationTests.java
@@ -1,9 +1,20 @@
 package com.neuroguard.paymentservice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+        "spring.datasource.url=jdbc:h2:mem:testdb",
+        "spring.datasource.username=sa",
+        "spring.datasource.password=",
+        "spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.H2Dialect",
+        "eureka.client.enabled=false",
+        "eureka.instance.enabled=false",
+        "spring.cloud.discovery.enabled=false"
+})
+@AutoConfigureTestDatabase(replace = Replace.ANY)
 class PaymentServiceApplicationTests {
 
     @Test
